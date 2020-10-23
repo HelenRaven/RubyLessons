0568aadabb45e03a04c97910497b333ad3e240eb
Elena Graivoronskaya <helenraven@mail.ru>
2020-10-23 12:05:19 +0700

    Lesson 1

---
+++ Lesson1/ideal_weight.rb
@@ -0,0 +1,13 @@
+puts "Как вас зовут? "
+name = gets.chomp.capitalize
+
+puts "Ваш рост (в сантиметрах)?"
+height = gets.to_i
+
+ideal_weight = (height - 110) * 1.15
+
+if ideal_weight > 0
+	puts "#{name}, ваш идеальный вес #{ideal_weight} кг."
+else
+	puts "#{name}, ваш вес оптимальный."
+end
---
+++ Lesson1/right_triangle.rb
@@ -0,0 +1,29 @@
+sides = []
+
+
+3.times do |i| 
+	puts "Введите длину #{i+1}й стороны треугольника:"
+	sides[i] = gets.to_f
+end
+
+if sides.uniq.length == 1
+	puts "Треугольник равносторонний (и по умолчанию - равнобедренный)"
+else
+# если треугольник не равносторонний, он может быть равнобедренным и/или прямоугольным
+	if sides.uniq.length == 2
+		puts "Треугольник равнобедренный"
+	end
+	
+	h = sides.max
+	katets = sides.reject{|x| x == h }
+	k_sqware = 0 
+	
+	katets.each {|katet| k_sqware = k_sqware + katet * katet}
+
+	if h*h == k_sqware 
+		puts "Треугольник прямоугольный"
+	end
+
+end
+
+		
